92f37843c741a1be255cde361bd9b02e22aed9c9
CCeph <yousefelsisyy@gmail.com>
2023-08-13 01:03:19 -0300

    Add merge sort common util
    Merge sort was used to sort the unsorted array that is passed to the
    buildTree factory

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import mergeSort from "./commonUtils.js";
+
+const splitArrayInHalfUtil = mergeSort.splitArrayInHalf;
+
 function createTreeNode(value, left = null, right = null) {
   return { value, left, right };
+}
+
+function buildTreeFromSortedArray(sortedArray) {
+  const { leftArray, rightArray } = splitArrayInHalfUtil(sortedArray);
+}
+
+function buildTree(array) {
+  const sortedArray = mergeSort.sort(array);
+  return sortedArray;
 }
