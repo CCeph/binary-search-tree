d577224b25a9c545adcd31634cf46c7fea6ca08f
CCeph <yousefelsisyy@gmail.com>
2023-08-13 01:06:11 -0300

    Remove splitArrayInHalf common util

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import mergeSort from "./commonUtils.js";
 
-const splitArrayInHalfUtil = mergeSort.splitArrayInHalf;
-
 function createTreeNode(value, left = null, right = null) {
   return { value, left, right };
 }
 
-function buildTreeFromSortedArray(sortedArray) {
-  const { leftArray, rightArray } = splitArrayInHalfUtil(sortedArray);
-}
+function buildTreeFromSortedArray(sortedArray) {}
 
 function buildTree(array) {
   const sortedArray = mergeSort.sort(array);
