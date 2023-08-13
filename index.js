da99009b5022af098f8c3ad3bf2e002635793564
CCeph <yousefelsisyy@gmail.com>
2023-08-13 01:27:38 -0300

    Create first draft of BST factory

--- index.js
+++ index.js
@@ -4,9 +4,34 @@ function createTreeNode(value, left = null, right = null) {
   return { value, left, right };
 }
 
-function buildTreeFromSortedArray(sortedArray) {}
+function buildTreeFromSortedArray(sortedArray) {
+  const start = 0;
+  const end = sortedArray.length - 1;
+  const midpoint = Math.floor((start + end) / 2);
+
+  if (start > end) {
+    return null;
+  }
+
+  const leftArray = sortedArray.slice(start, midpoint);
+  const rightArray = sortedArray.slice(midpoint + 1, end + 1);
+
+  const leftNode = buildTreeFromSortedArray(leftArray);
+  const rightNode = buildTreeFromSortedArray(rightArray);
+
+  const currentNode = createTreeNode(
+    sortedArray[midpoint],
+    leftNode,
+    rightNode
+  );
+  return currentNode;
+}
 
 function buildTree(array) {
   const sortedArray = mergeSort.sort(array);
   return sortedArray;
 }
+
+const testArray = [1, 3, 5, 7, 8, 9, 10];
+
+console.log(buildTreeFromSortedArray(testArray));
