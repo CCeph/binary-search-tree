31f30b478b5784da369b71b696ff26fcea3007cb
CCeph <yousefelsisyy@gmail.com>
2023-08-13 15:02:20 -0300

    Encapsulate everything to do with BST in an object
    Given that these functions will likely be used as utilities in future
    projects, they should be easily exportable.

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 // eslint-disable-next-line import/extensions
 import mergeSort from "./commonUtils.js";
 
-function createTreeNode(data, left = null, right = null) {
-  return { data, left, right };
-}
+const binarySearchTree = {
+  createTreeNode(data, left = null, right = null) {
+    return { data, left, right };
+  },
 
-function buildTreeFromSortedArray(sortedArray) {
-  const start = 0;
-  const end = sortedArray.length - 1;
-  const midpoint = Math.floor((start + end) / 2);
+  buildTreeFromSortedArray(sortedArray) {
+    const start = 0;
+    const end = sortedArray.length - 1;
+    const midpoint = Math.floor((start + end) / 2);
 
-  if (start > end) {
-    return null;
-  }
+    if (start > end) {
+      return null;
+    }
 
-  const leftArray = sortedArray.slice(start, midpoint);
-  const rightArray = sortedArray.slice(midpoint + 1, end + 1);
+    const leftArray = sortedArray.slice(start, midpoint);
+    const rightArray = sortedArray.slice(midpoint + 1, end + 1);
 
-  const leftNode = buildTreeFromSortedArray(leftArray);
-  const rightNode = buildTreeFromSortedArray(rightArray);
+    const leftNode = this.buildTreeFromSortedArray(leftArray);
+    const rightNode = this.buildTreeFromSortedArray(rightArray);
 
-  const currentNode = createTreeNode(
-    sortedArray[midpoint],
-    leftNode,
-    rightNode
-  );
-  return currentNode;
-}
+    const currentNode = this.createTreeNode(
+      sortedArray[midpoint],
+      leftNode,
+      rightNode
+    );
+    return currentNode;
+  },
 
-function buildTree(array) {
-  const sortedArray = mergeSort.sort(array);
-  return sortedArray;
-}
+  buildTree(array) {
+    const sortedArray = mergeSort.sort(array);
+    return sortedArray;
+  },
+};
 
 const testArray = [1, 3, 5, 7, 8, 9, 10];
 
-console.log(buildTreeFromSortedArray(testArray));
+console.log(binarySearchTree.buildTreeFromSortedArray(testArray));
